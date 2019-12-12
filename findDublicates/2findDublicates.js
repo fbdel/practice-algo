300d767b0d48236b73011bc2b3b2a4fee52c1988
Fadil Abdella <fabdella03@gmail.com>
2019-12-12 16:32:21 -0500

    FindDubli 2

---
+++ findDublicates/2findDublicates.js
@@ -0,0 +1,20 @@
+//Find and Return dublicates in an array
+//Nested iterations 
+
+function findDublicates(arr) {
+    const dublicates = []
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                if (!dublicates.includes(arr[j])) {
+                    dublicates.push(arr[i])
+                }
+            }
+        }
+    }
+    console.log(dublicates)
+    return dublicates
+}
+
+findDublicates([1, 1, 1, 3, 2, 2, 4, 5, 5])
--- findPairs/play.js
+++ findPairs/play.js
@@ -24,26 +24,6 @@ function sockMerchant(n, ar) {
 sockMerchant(9, [1, 1, 1, 3, 3, 2, 2, 4, 5, 5]);
 
 
-//Find and Return dublicates in an array
-//Create an object and return values > 1
-
-function findDublicates(arr) {
-  const intObj = {}
-  let dublicates = []
-  
-  for(let int of arr){
-    intObj[int] = intObj[int] +1 || 1;
-  }
-
-  for(let key in intObj){
-    if(intObj[key] > 1){
-      dublicates.push(key)
-    }
-  }
-  console.log(dublicates)
-  return dublicates
-}
-findDublicates([1, 1, 1, 3, 2, 2, 4, 5, 5,9])
 
 
 
@@ -52,25 +32,4 @@ findDublicates([1, 1, 1, 3, 2, 2, 4, 5, 5,9])
 
 
 
-
-
-//Find and Return dublicates in an array
-//Nested iterations 
-
-function findDublicates(arr){
-  const dublicates= []
-
-  for(let i=0; i<arr.length; i++){
-    for(let j = i+1; j<arr.length;j++){
-      if (arr[i]===arr[j]){
-        if (!dublicates.includes(arr[j])){
-          dublicates.push(arr[i])
-        }
-      }
-    }
-  }
-  console.log(dublicates)
-  return dublicates
-}
-findDublicates([1, 1, 1, 3, 2, 2, 4, 5, 5])
 
